14372221b215d37a9cafc9829c4f466193689bde
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-07-30 00:34:16 -0300

    mongodb

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,10 +3,23 @@ const route = express.Router();
 
 const services = require("../services/render");
 
-//
+/**
+ * @description Home page
+ * @method GET /
+ */
+
 route.get("/", services.homeRoutes);
 
+/**
+ * @description add users
+ * @method GET /add-users
+ */
+
 route.get("/add-user", services.add_user);
+/**
+ * @description update users
+ * @method GET /update-users
+ */
 
 route.get("/update-user", services.update_user);
 
