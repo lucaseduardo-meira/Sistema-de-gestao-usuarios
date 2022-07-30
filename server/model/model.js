59206fb4e5fa81672bd1e9817febe474c97fd946
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-07-30 01:15:08 -0300

    api

---
+++ server/model/model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+var schema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  gender: String,
+  status: String,
+});
+
+const Userdb = mongoose.model("userdb", schema);
+
+module.exports = Userdb;
