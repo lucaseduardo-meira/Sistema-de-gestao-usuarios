c33ad4e6e39ab30d22d7e6932588da85c665e04f
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-07-31 23:24:09 -0300

    Update index.js

--- server/database/index.js
+++ server/database/index.js
@@ -1,2 +1,8 @@
 const Sequelize = require("sequelize");
 const dbConfig = require("../database");
+
+const User = require("../model/User");
+
+const connection = new Sequelize(dbConfig);
+
+module.exports = connection;
