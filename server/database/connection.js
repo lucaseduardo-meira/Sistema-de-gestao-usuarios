d7faa6ed3974d6a7332f893387ffd265cf210d3c
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-07-31 22:00:23 -0300

    mysql login

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,11 +1,10 @@
-const dotenv = require("dotenv");
-
 module.exports = {
   dialect: "mysql",
   host: "localhost",
   username: "root",
-  password: "",
+  password: "lucasdd2004",
   database: "crudsql",
+  port: 3306,
   define: {
     timestamps: true,
     underscored: true,
