cfca8464c87a004d563c84ff9d0827248a99a758
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-07-31 21:22:07 -0300

    Update connection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,8 +3,8 @@ const dotenv = require("dotenv");
 module.exports = {
   dialect: "mysql",
   host: "localhost",
-  username: "crudproject",
-  password: "crudpassword",
+  username: "root",
+  password: "",
   database: "crudsql",
   define: {
     timestamps: true,
