feb36f99501ed7ada5d0423f0c048a7f985d9c44
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-08-02 01:27:42 -0300

    Update 20220801050755-delete-table.js

--- server/database/migrations/20220801050755-delete-table.js
+++ server/database/migrations/20220801050755-delete-table.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("users", "upadated_at");
+    await queryInterface.removeColumn("users", "updated_at");
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("users", "updated_at");
+    await queryInterface.addColumn("users", "upadated_at");
   },
 };
