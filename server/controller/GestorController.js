2b046dca7bfee9f805ab24109373cdbbbbbbfae7
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-08-02 23:46:15 -0300

    Update GestorController.js

--- server/controller/GestorController.js
+++ server/controller/GestorController.js
@@ -1,5 +1,30 @@
+const User = require("../model/User");
 const Gestor = require("../model/Gestor");
+const session = require("express-session");
 
 module.exports = {
-  async store(req, res) {},
+  async store(req, res) {
+    const { user } = req.session.login;
+    const { name, email, gender, status } = req.body;
+
+    const user_id = await User.findAll({
+      attributes: "user_id",
+      where: {
+        name: name,
+      },
+    });
+    if (!user_id) {
+      return res.status(400).json({ error: "user not found" });
+    }
+
+    const gestor = await Gestor.create({
+      name,
+      email,
+      gender,
+      status,
+      user_id,
+    });
+
+    return res.json(gestor);
+  },
 };
