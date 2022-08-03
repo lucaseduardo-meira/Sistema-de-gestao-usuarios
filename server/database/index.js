d8ceb9f8c98f96a7ac163f3f7233e0dbd5b2f90d
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-08-03 00:23:25 -0300

    Update index.js

--- server/database/index.js
+++ server/database/index.js
@@ -2,12 +2,13 @@ const Sequelize = require("sequelize");
 const dbConfig = require("../config/database");
 
 const User = require("../model/User");
-const gestor = require("../model/Gestor");
 const Gestor = require("../model/Gestor");
 
 const connection = new Sequelize(dbConfig);
 
 User.init(connection);
 Gestor.init(connection);
+
+Gestor.associate(connection.models);
 
 module.exports = connection;
