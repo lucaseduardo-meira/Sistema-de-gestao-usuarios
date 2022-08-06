5ac8fea0bb9ff39e900942950e33d2ab9c3fc12c
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-08-06 01:33:43 -0300

    jquery not loading

--- assets/js/delete.js
+++ assets/js/delete.js
@@ -1,10 +1,4 @@
-const path = require("path");
-const GestorController = require(path.resolve(
-  __dirname,
-  "server/controller/GestorController"
-));
-
 if (window.location.pathname == "/") {
-  $ondelete = $(".table tbody td a.delete");
-  $ondelete.click(GestorController.delete);
+  $ondelete = $("#delete");
+  $ondelete.click(console.log("click"));
 }
