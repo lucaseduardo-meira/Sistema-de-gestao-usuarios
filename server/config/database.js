38c774353d0e05bc2e75e837e7913f139f7fddff
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-12-13 23:10:14 -0300

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: "mysql",
-  host: "host",
-  username: "username",
-  password: "password",
-  database: "database",
+  host: "sql101.epizy.com",
+  username: "epiz_33195929",
+  password: "ijLl2NwtNyo",
+  database: "epiz_33195929_sistema_de_gestao",
   port: 3306,
   define: {
     timestamps: true,
